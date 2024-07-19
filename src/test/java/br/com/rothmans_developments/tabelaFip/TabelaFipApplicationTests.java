4dadfba3bb602b70f26041ee83682c806d49e6d7
nelsonFiho22222 <nelsoncamarafilho@gmail.com>
2024-07-19 17:06:39 -0300

    Primeiro Commit

---
+++ src/test/java/br/com/rothmans_developments/tabelaFip/TabelaFipApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rothmans_developments.tabelaFip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabelaFipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
